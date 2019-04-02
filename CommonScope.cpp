c117f0f5e7881783a0b75c14d9ba7e751b6b298a
devmichalek <devmichalek@gmail.com>
2019-04-02 21:07:31 +0200

    New Common Scope

---
+++ CommonScope.cpp
@@ -0,0 +1,51 @@
+#include "CommonScope.h"
+
+CommonScope* CommonScope::getNextNode()
+{
+	return nullptr;
+}
+
+void CommonScope::print(std::string &&msg, int &&)
+{
+	std::lock_guard<std::mutex> locker(m_omutex);
+	printf("%s", msg.c_str());
+}
+
+void CommonScope::redirect(std::string &&msg, int &&index)
+{
+	std::lock_guard<std::mutex> locker(m_romutexes[index]);
+	if (m_rofiles[index].first->is_open())
+		*m_rofiles[index].first << msg;
+}
+
+void CommonScope::push(Command* &cmd, std::string &pathToFile)
+{
+	unsigned output_index = -1;
+	if (!pathToFile.empty())
+	{	// open next file...
+		auto it = m_rofiles.begin();
+		for (; it != m_rofiles.end(); ++it)
+			if (it->second == pathToFile)
+				break;
+
+		if (it == m_rofiles.end())
+		{
+			std::fstream* file = new std::fstream;
+			file->open(pathToFile, std::ios::app);
+			output_index = (int)m_rofiles.size();
+			m_rofiles.push_back(std::make_pair(file, pathToFile));
+		}
+		else
+			output_index = (int)std::distance(m_rofiles.begin(), it);
+	}
+
+	// add next task...
+	void(*fun)(std::string &&, int &&) = output_index < 0 ? &print : &redirect;
+	m_tasks.push(std::make_pair(cmd, (void*)fun));
+}
+
+void CommonScope::pop()
+{
+	delete m_tasks.front().first;
+	m_tasks.pop();
+}
---
+++ CommonScope.h
@@ -0,0 +1,44 @@
+#pragma once
+#include "cmd.h"
+#include <queue>
+#include <mutex>
+#include <thread>
+#include <fstream>
+
+class CommonScope
+{
+protected:
+	// task manipulation
+	typedef std::pair<Command*, void*> m_task_type;
+	std::queue<m_task_type> m_tasks;
+
+	// output manipulation
+	inline static std::mutex m_omutex;
+
+	// redirected output manipulation
+	using m_rofile_type = std::pair<std::fstream*, std::string>;
+	inline static std::vector<m_rofile_type> m_rofiles;
+	inline static std::vector<std::mutex> m_romutexes;
+
+public:
+	enum M_TYPE
+	{
+		REGULAR = 0,
+		CONCURRENT
+	};
+	M_TYPE m_type;
+
+	explicit CommonScope(const M_TYPE &newType) { m_type = newType; }
+	~CommonScope() {}
+
+	virtual bool addTask(Command*, std::string) = 0;
+	virtual bool addScope(CommonScope*, M_TYPE) = 0;
+	virtual bool execute() = 0;
+	virtual CommonScope* getNextNode();
+
+protected:
+	static void print(std::string &&, int &&);
+	static void redirect(std::string &&, int &&);
+	void push(Command*&, std::string &);
+	void pop();
+};
