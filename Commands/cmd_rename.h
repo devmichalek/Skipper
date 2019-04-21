3dff9ab21e36c97182beaa8c7ca6819f7dcd7411
devmichalek <devmichalek@gmail.com>
2019-04-21 22:38:04 +0200

    New command Rename

---
+++ Commands/cmd_rename.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "cmd.h"
+
+class Command_Rename final : public Command
+{
+	bool m_bEmpty;
+	bool m_bHelp;
+	bool m_bDirectory;
+	bool m_bVerbose;
+	bool m_bRecursive;
+	bool m_bRegex;
+	std::string m_sDirectory;
+	std::string m_sRegex;
+
+public:
+	explicit Command_Rename(std::vector<std::string> options);
+	~Command_Rename() {} // not needed, no objects on heap
+	bool parse();
+	int run();
+
+	static std::string help()
+	{
+		std::string a = "\nDefinition:\n";
+		std::string b = "\trename - renames files\n";
+		std::string c = "\nSyntax:\n";
+		std::string d = "\t[-h --help] - prints help\n";
+		std::string e = "\t[-f --directory <directory name>] - searches in requested directory, if not specified searches in current directory\n";
+		std::string f = "\t[-r --recursive] - searches recursively\n";
+		std::string g = "\t[<regular expression> <regular expression>] - searches directory with specified regular expression keys\n";
+		std::string h = "\n";
+		return a + b + c + d + e + f + g + h;
+	}
+
+	static std::string assist()
+	{
+		return "  rename\n\t[-h --help]\n\t[-d --directory <directory name>]\n\t[-r --recursive]\n\t[<regular expression> <regular expression>]\n";
+	}
+};
